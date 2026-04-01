a6a1f68bac867c55c1de14734d5b6d0879774b77
Irene011878 <IA00833195@TechMahindra.com>
2026-04-01 00:08:15 -0600

    Fix Chrome headless for CI

--- src/test/java/driver/DriveManager.java
+++ src/test/java/driver/DriveManager.java
@@ -2,14 +2,27 @@ package driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriveManager {
 
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
-    public static void setDriver(){
+    public static void setDriver() {
 
-        driver.set(new ChromeDriver());
+        //driver.set(new ChromeDriver());
+        ChromeOptions options = new ChromeOptions();
+
+        // Detectar si estamos en CI
+        String isCI = System.getenv("CI");
+
+        if (isCI != null) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+        }
+        driver.set(new ChromeDriver(options));
     }
 
     public static WebDriver getDriver(){
